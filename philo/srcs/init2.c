34f5215faebcbcd692dc917a24e2e976a29e27f2
Nic Flenghi <nic.flenghi@gmail.com>
2022-02-28 15:20:47 +0900

    fix mutex destroy order in clean_up_threads

--- philo/srcs/init2.c
+++ philo/srcs/init2.c
@@ -6,7 +6,7 @@
 /*   By: fnichola <fnichola@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 17:41:14 by fnichola          #+#    #+#             */
-/*   Updated: 2022/02/24 17:42:43 by fnichola         ###   ########.fr       */
+/*   Updated: 2022/02/28 15:11:38 by fnichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,13 @@ void	clean_up_threads(t_data *data, t_philo **philos)
 	while (i < data->nbr_of_philos)
 	{
 		pthread_join((*philos)[i].thread, NULL);
-		pthread_mutex_destroy(&data->forks[i]);
+		i++;
+	}
+	i = 0;
+	while (i < data->nbr_of_philos)
+	{
 		pthread_mutex_destroy(&(*philos)[i].eating_mtx);
+		pthread_mutex_destroy(&data->forks[i]);
 		i++;
 	}
 	pthread_mutex_destroy(&data->philo_died_mtx);
